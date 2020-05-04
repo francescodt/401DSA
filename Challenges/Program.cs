d828e166e46d178220a0505eb3c842e30b4a458c
Francesco <francescodtroiano@gmail.com>
2020-05-04 14:37:06 -0500

    Scaffolding?

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -6,7 +6,23 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            /* for (int i = arr.Length -1; i >= 0; i--)
+             * {
+             *      Console.Write(arr[i]);
+             * }
+             */
+            int length = arr.Length - 1;
+            string strReverse = null;
+
+            while (length >= 0)
+            {
+                strReverse = strReverse + arr[length];
+                length--;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Reverse Array is " + " " + strReverse);
+            Console.ReadLine();
         }
     }
 }
