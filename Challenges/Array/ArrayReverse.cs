47edcb762882aff8e9ddfd9a866145bd6288af75
Francesco <francescodtroiano@gmail.com>
2020-05-06 19:26:34 -0500

    tests and search working. did it all in one go. perfectly. no issues, unless I made them on purpose for my tests. god will stand witness. quit looking at my commits, Keith

--- Challenges/Array/ArrayReverse.cs
+++ Challenges/Array/ArrayReverse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Challenges.Array
+﻿namespace Challenges.Array
 {
     public class ArrayReverse
     {
---
+++ Challenges/Array/BinaryArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Array
+{
+    public class BinaryArray
+    {
+        public static int ArrayBinary(int[] array, int insertedValue)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int notFound = -1;
+
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if (insertedValue == array[mid])
+                {
+                    return mid;
+                }
+                else if (insertedValue < array[mid])
+                {
+                    high = mid - 1;
+                }
+                else if (insertedValue > array[mid])
+                {
+                    low = mid + 1;
+                }
+                         
+            }
+            return notFound;
+        }
+    }
+}
