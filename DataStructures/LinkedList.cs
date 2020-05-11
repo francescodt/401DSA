ee328b6d1384042d0b68aaef42fe39785111f3fd
Francesco <francescodtroiano@gmail.com>
2020-05-11 15:44:36 -0500

    creating head

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public override string ToString()
+        {
+            if (Head == null)
+            {
+                return "";
+
+            }
+            else
+            {
+                Node current = Head;
+                string list = "";
+
+                while (current != null)
+                {
+                    list += "{ ";
+                    list += current.ToString();
+                    current = current.Next;
+                    if (current != null)
+                    {
+                        list += " } -> ";
+                    }
+                }
+
+                list += " } -> Null";
+
+                return list;
+            }
+        }
+
+
+    }
+}
