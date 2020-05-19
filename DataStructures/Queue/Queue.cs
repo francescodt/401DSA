ee437144f30121827621c92f154fae03598890e9
Francesco <francescodtroiano@gmail.com>
2020-05-19 09:04:44 -0500

    just queues, no stacks, enqueue

---
+++ DataStructures/Queue/Queue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace DataStructures.Queue
+{
+    public class Queue<T>
+    {
+        private Node front;
+        private Node rear;
+
+        public T Enqueue(T value)
+        {
+            Node newRear = new Node(value);
+
+            if (rear == null)
+            {
+                front = rear = newRear;
+                return rear.Value;
+            }
+
+            rear.Next = newRear;
+            rear = newRear;
+
+            return rear.Value;
+        }
+
+        public class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+
+            public T Value { get; set; }
+            public Node Next { get; set; }
+
+        }
+        
+    }
+}
