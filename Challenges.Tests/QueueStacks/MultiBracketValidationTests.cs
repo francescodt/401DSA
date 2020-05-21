89119d828354374358a3c09722a11d1067ab0865
Francesco <francescodtroiano@gmail.com>
2020-05-20 22:34:59 -0500

    tests

---
+++ Challenges.Tests/QueueStacks/MultiBracketValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using Challenges.QueueStacks;
+using Xunit;
+
+namespace Challenges.Tests.QueueStacks
+{
+    public class MultiBracketValidationTests
+    {
+        [Theory]
+        [InlineData("{}", true)]
+        [InlineData("{", false)]
+        [InlineData("(())", true)]
+        [InlineData("{{enfj}}", true)]
+        [InlineData("{{(yup)})", false)]
+        [InlineData("[puy[", false)]
+        public void MultiSupportBracing(string brace, bool expected)
+        {
+            bool result = MultiBracketValidation.braceCheck(brace);
+
+            Assert.Equal(result, expected);
+        }
+    }
+}
