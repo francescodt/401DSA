f51469d61892da944757015772bdb3f68e243538
Francesco <francescodtroiano@gmail.com>
2020-05-25 11:21:43 -0500

    Search tree class added, add added

---
+++ DataStructures/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace DataStructures.BinaryTree
+{
+    public class BinarySearchTree<T> : BinaryTree<T>
+        where T : IComparable<T>
+    {
+
+        public void Add(T value)
+        {
+
+            Node newNode = new Node(value);
+            Node current = Root;
+            Node parent = null;
+
+            if (Root == null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    parent = current;
+                    if (value.CompareTo(current.Value) < 0)
+                    {
+                        current = current.Left;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                    }
+                }
+
+                if (value.CompareTo(parent.Value) < 0)
+                {
+                    parent.Left = newNode;
+                }
+                else
+                {
+                    parent.Right = newNode;
+                }
+
+                
+            }
+        }
+    }
+}
