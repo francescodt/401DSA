7d523a9e4f5a7cfb995f217ed2fe228555057717
Francesco <francescodtroiano@gmail.com>
2020-05-25 13:58:38 -0500

    Preorder and Adding tests added

--- DataStructures.Tests/BinaryTreeTests.cs
+++ DataStructures.Tests/BinaryTreeTests.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructures.BinaryTree;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -15,7 +16,50 @@ namespace DataStructures.Tests
 
             Assert.Null(binaryTree.Root);
         }
+
+        [Fact]
+        public void AddToTree()
+        {
+            BinarySearchTree<int> newNode = new BinarySearchTree<int>();
+
+            newNode.Add(1);
+            int expected = 1;
+
+            Assert.Equal(expected, newNode.Root.Value);
+        }
+
+        [Fact]
+        public void AddLeftChildRightChildToSingleRootNode()
+        {
+            BinarySearchTree<int> newNode = new BinarySearchTree<int>();
+
+            newNode.Add(2);
+            newNode.Add(1);
+            newNode.Add(3);
+
+            Assert.Equal(2, newNode.Root.Value);
+            Assert.Equal(1, newNode.Root.Left.Value);
+            Assert.Equal(3, newNode.Root.Right.Value);
+        }
+
+        [Fact]
+        public void CanReturnPreOrder()
+        {
+            BinarySearchTree<int> newNode = new BinarySearchTree<int>();
+            newNode.Add(7);
+            newNode.Add(4);
+            newNode.Add(8);
+            newNode.Add(1);
+
+            
+            Assert.True(newNode.Contains(newNode.Root, 7));
+            Assert.True(newNode.Contains(newNode.Root.Left, 4));
+            Assert.True(newNode.Contains(newNode.Root.Right, 8));
+            Assert.True(newNode.Contains(newNode.Root.Left.Left, 1));
+        }
     }
+
+    
 
 
 }
