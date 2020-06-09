19389b4673d58ca736f0b7b611a4fdf997ad6af3
Francesco <francescodtroiano@gmail.com>
2020-06-08 23:04:33 -0500

    tests now passing, were failing

--- Challenges/BaseBaseBase/InsertionSort.cs
+++ Challenges/BaseBaseBase/InsertionSort.cs
@@ -6,19 +6,20 @@ namespace Challenges.BaseBaseBase
 {
         public class InsertionSort
         {
-            public static int[] InserstionSortMethod(int [] arr)
+            public static int[] InserstionSortMethod(int[] arr)
             {
-                for (int i = 0; i < arr.Length; i++)
+                for (int i = 1; i < arr.Length; i++)
                 {
                     int j = i - 1;
-                    int temp = arr[i];
+                    int curr = arr[i];
 
-                    while(j >= 0 && temp < arr[j])
+                    while(j >= 0 && curr < arr[j])
                     {
-                        arr[j + i] = arr[j];
-                        j = j + i;
-                        arr[j + 1] = temp;
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                        
                     }
+                    arr[j + 1] = curr;
                 }
                 return arr;
             }
