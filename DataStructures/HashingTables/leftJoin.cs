05a95815dccae9ab8e5a13a1fabd27f8df18b42f
Francesco <francescodtroiano@gmail.com>
2020-06-21 21:29:32 -0500

    something I guess

---
+++ DataStructures/HashingTables/leftJoin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.HashingTables
+{
+    public class leftJoin
+    {
+        public static HashSet<string[]> LeftJoin(HashTable left, HashTable right)
+        {
+            HashSet<string[]> leftHash = new HashSet<string[]>();
+            HashSet<string[]> result = new HashSet<string[]>();
+
+            foreach (string[] word in left)
+            {
+                leftHash.Add(word.Split(','));
+            }
+
+            foreach (string[] entry in left)
+            {
+                if (right.HashExists(entry[0]))
+                {
+                    result.Add(new string[] { entry[0], entry[1], right.GetFromTable(entry[0]) });
+                }
+                else
+                {
+                    result.Add(new string[] { entry[0], entry[1], "null" });
+                }
+            }
+            return result;
+        }
+    }
+}
