0741f080102f3c78fdee3ab13a734956ec32bc57
Francesco <francescodtroiano@gmail.com>
2020-06-22 18:56:00 -0500

    anotha one

--- DataStructures.Tests/GraphTesting.cs
+++ DataStructures.Tests/GraphTesting.cs
@@ -17,6 +17,15 @@ namespace DataStructures.Tests
             Assert.Null(newGraph.GetNodes());
         }
 
+        [Fact]
+        public void VerticiesAreCool()
+        {
+            Graph<int> newGraph = new Graph<int>();
+            newGraph.AddNode(2);
+            
 
+            Assert.Contains(2, newGraph.GetNodes());
+
+        }
     }
 }
