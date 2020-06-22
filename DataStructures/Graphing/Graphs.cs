959c7a150d35dc3ee05875c3076f9d5b8ff2df78
Francesco <francescodtroiano@gmail.com>
2020-06-21 22:57:56 -0500

    Logic

---
+++ DataStructures/Graphing/Graphs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures.Graphing
+{
+    public class Graph<T>
+    {
+        Dictionary<T, Dictionary<T, int>> Nodes;
+        public Graph()
+        {
+            Nodes = new Dictionary<T, Dictionary<T, int>>();
+        }
+
+        public bool AddNode(T value)
+        {
+            if (Nodes.ContainsKey(value)) return false;
+
+            Nodes.Add(value, new Dictionary<T, int>());
+            return true;
+        }
+
+        public bool AddEdge(T firstNodeValue, T secondNodeValue, int weight = 1, bool oneWay = false)
+        {
+
+            if (!Nodes.ContainsKey(firstNodeValue) || !Nodes.ContainsKey(secondNodeValue)) return false;
+
+            Dictionary<T, int> nodeOne = Nodes[firstNodeValue];
+            Dictionary<T, int> nodeTwo = Nodes[secondNodeValue];
+
+            if (nodeOne.ContainsKey(firstNodeValue)) return false;
+
+            if (!oneWay && !firstNodeValue.Equals(secondNodeValue))
+            {
+                if (nodeTwo.ContainsKey(firstNodeValue)) return false;
+                nodeTwo.Add(firstNodeValue, weight);
+            }
+
+
+            nodeOne.Add(secondNodeValue, weight);
+            return true;
+        }
+
+        public List<T> GetNodes()
+        {
+            if (Nodes.Keys.Count < 1) return null;
+            return Nodes.Keys.ToList();
+        }
+
+        public Dictionary<T, int> GetNeighbors(T value)
+        {
+            if (!Nodes.ContainsKey(value)) return null;
+
+            return Nodes[value];
+        }
+
+        public int Size()
+        {
+            return Nodes.Keys.Count;
+        }
+    }
+}
