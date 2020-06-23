d6814dc57df3f328c27bd380eda3718af5531f27
Francesco <francescodtroiano@gmail.com>
2020-06-22 19:03:12 -0500

    weight being tested

--- DataStructures.Tests/GraphTesting.cs
+++ DataStructures.Tests/GraphTesting.cs
@@ -43,5 +43,19 @@ namespace DataStructures.Tests
             Assert.Contains(3, newGraph.GetNodes());
 
         }
+
+        [Fact]
+        public void WeightIsBadMmkay()
+        {
+            Graph<int> newGraph = new Graph<int>();
+            newGraph.AddNode(1);
+            newGraph.AddNode(2);
+            newGraph.AddEdge(1, 2, 3);
+
+            Assert.Equal(3, newGraph.GetNeighbors(1).GetValueOrDefault(2));
+        }
+
+        [Fact]
+        
     }
 }
