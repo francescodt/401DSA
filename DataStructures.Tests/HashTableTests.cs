e8ad27039f59a6cf476a7e5b3b140494b14a5463
Francesco <francescodtroiano@gmail.com>
2020-06-23 19:12:35 -0500

    Words exist, and hashing is good tests

---
+++ DataStructures.Tests/HashTableTests.cs
@@ -0,0 +1,38 @@
+﻿using DataStructures.HashingTables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class HashTableTests
+    {
+
+        [Fact]
+        public void CanHash()
+        {
+            string key = "Cat";
+
+            int result = HashTable.HashCode(key);
+
+            Assert.Equal(14, result);
+        }
+
+        [Fact]
+        public void WordsCanExist()
+        {
+            HashTable newTable = new HashTable(99);
+
+            newTable.AddHash("dog", "cat");
+
+            Assert.True(newTable.HashExists("dog"));
+        }
+
+        [Fact]
+        public void CanAddKeyValues()
+        {
+            HashTable newTable = new HashTable()
+        }
+    }
+}
